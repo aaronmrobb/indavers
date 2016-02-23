8e1318e38979d6ae4a988f06aad5e7a18e9ff30a
Aaron Robb <airrobb@gmail.com>
2016-02-23 15:42:40 +1300

    stupid merge

--- src/reducers/next.js
+++ src/reducers/next.js
@@ -8,7 +8,11 @@ export default function next(state) {
 function addWordRow(state) {
   const nextState = state.set('board', state.get('board').unshift(
     fromJS(new Array(5).fill('').map((cell) => {
-      return Math.random() > 0.5 ? '' : randomWords()
+      let newInvader = 'new blank word'
+      while(newInvader.length > 7) {
+        newInvader = randomWords()
+      }
+      return Math.random() > 0.4 ? '' : newInvader
     }))
   ))
   return checkForLoss(nextState)
