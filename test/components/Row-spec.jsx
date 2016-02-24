06b1edb816021942348a0fb575587bb983ee20a9
Aaron Robb <airrobb@gmail.com>
2016-02-24 14:33:33 +1300

    Row tests

--- test/components/Row-spec.jsx
+++ test/components/Row-spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
-import { Map } from 'immutable'
+import { Map, List } from 'immutable'
 
 chai.use(chaiEnzyme())
 
@@ -9,9 +9,31 @@ import { mount, render, shallow } from 'enzyme'
 import { Row } from '../../src/components/Row.jsx'
 
 describe('<Row />', () => {
-  it('has class of row')
-  it('has 5 cells')
-  it('has correct amount of cells with invaders')
-  it('has cells with correct invader text')
-  it('fades when game is over')
+  it('has class of row', () => {
+    const content = List.of('Bub', 'bob', 'bill', 'bingo', '')
+    const wrapper = shallow(<Row cells={content}/>)
+    expect(wrapper).to.have.className('row')
+  })
+  it('has 5 cells', () => {
+    const content = List.of('Bub', 'bob', 'bill', 'bingo', '')
+    const wrapper = shallow(<Row cells={content}/>)
+    expect(wrapper.find('Cell').length).to.equal(5)
+  })
+  it('has correct amount of cells with invaders', () => {
+    const content = List.of('Bub', 'bob', 'bill', 'bingo', '')
+    const wrapper = mount(<Row cells={content}/>)
+    expect(wrapper.find('.invader').length).to.equal(4)
+  })
+  it('has cells with correct invader text', () => {
+    const content = List.of('Bub', 'bob', 'bill', 'bingo', '')
+    const wrapper = mount(<Row cells={content}/>)
+    expect(wrapper.find('.invader').first()).to.have.text('Bub')
+    expect(wrapper.find('.invader').last()).to.have.text('bingo')
+
+  })
+  it('fades when game is over', () => {
+    const content = List.of('Bub', 'bob', 'bill', 'bingo', '')
+    const wrapper = mount(<Row cells={content} gameover={true}/>)
+    expect(wrapper).to.have.style('opacity', '0.3')
+  })
 })
