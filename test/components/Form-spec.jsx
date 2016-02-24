cd3226b78425b57d1149b42a452daa7bb41f33f3
Aaron Robb <airrobb@gmail.com>
2016-02-24 14:46:53 +1300

    form tests

--- test/components/Form-spec.jsx
+++ test/components/Form-spec.jsx
@@ -7,3 +7,30 @@ chai.use(chaiEnzyme())
 
 import { mount, render, shallow } from 'enzyme'
 import { Form } from '../../src/components/Form.jsx'
+
+describe('<Form />', () => {
+  it('renders without props', () => {
+    const wrapper = shallow(<Form />)
+    expect(wrapper).to.be.ok
+    expect(wrapper.find('input')).to.be.ok
+    expect(wrapper.find('form')).to.be.ok
+  })
+  it('renders with correct class and id', () => {
+    const wrapper = shallow(<Form />)
+    expect(wrapper.find('.shooter')).to.be.ok
+    expect(wrapper.find('#gun')).to.be.ok
+  })
+  it('input has placeholder text', () => {
+    const wrapper = shallow(<Form />)
+    expect(wrapper.find('#gun')).to.have.attr('placeholder', 'Shoot shit')
+
+  })
+  it('is not disabled while game is being played', () => {
+    const wrapper = shallow(<Form gameover={false}/>)
+    expect(wrapper.find('#gun')).to.not.have.attr('disabled')
+  })
+  it('is disabled when game is over', () => {
+    const wrapper = shallow(<Form gameover={true}/>)
+    expect(wrapper.find('#gun')).to.have.attr('disabled')
+  })
+})
