a1ce8747d7871e719fae2ca7521b9806ebd58b2f
Aaron Robb <airrobb@gmail.com>
2016-02-25 22:30:44 +1300

    clean up shoot-word

--- src/reducers/shoot-word.js
+++ src/reducers/shoot-word.js
@@ -18,10 +18,8 @@ function findWord(state, word) {
 
 function checkShotPath(state, coordinates, word) {
   let firstHit
-  state.get('board').map((row) => {
-    row.map((cell, idx) => {
-       if (idx === coordinates.get(1) && cell.length > 0) { firstHit = cell }
-    })
-  })
+  state.get('board').map((row) => { row.map((cell, idx) => {
+    if (idx === coordinates.get(1) && cell.length > 0) { firstHit = cell }
+  })})
   return firstHit === word
 }
